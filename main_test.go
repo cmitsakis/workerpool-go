0c92421c31ebcba90b02d42db4db3333d19c0c98
Charalampos Mitsakis <c@mitsakis.org>
2022-04-29 18:02:14 +0300

    tests: fail on unexpected number of workers

--- main_test.go
+++ main_test.go
@@ -25,63 +25,22 @@ func TestExample(t *testing.T) {
 	p.StopAndWait()
 }
 
-func TestPoolSimple(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())
-	results := make(chan int)
-	p, err := NewPoolSimple(2, func(job Job[int], workerID int) error {
-		if rand.Float32() > 0.95 {
-			return ErrorWrapRetryable(fmt.Errorf("job failure"))
-		}
-		results <- 2 * job.Payload
-		return nil
-	}, Retries(4), IdleTimeout(1*time.Minute), LoggerInfo(log.Default()), LoggerDebug(log.Default()))
-	if err != nil {
-		log.Printf("NewPoolSimple() failed: %s", err)
-		return
-	}
-	go func() {
-		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
-		defer cancel()
-	loop:
-		for i := 0; i < 100000; i++ {
-			select {
-			case <-ctx.Done():
-				break loop
-			default:
-			}
-			log.Printf("[test] submitting job%d\n", i)
-			p.Submit(i)
-		}
-		log.Println("[test] submitted jobs - calling p.StopAndWait()")
-		p.StopAndWait()
-		log.Println("[test] p.StopAndWait() returned")
-		close(results)
-	}()
-	const a = 0.1
-	var outputPeriodAvg time.Duration
-	lastReceived := time.Now()
-	for range results {
-		outputPeriod := time.Since(lastReceived)
-		lastReceived = time.Now()
-		outputPeriodAvg = time.Duration(a*float64(outputPeriod) + (1-a)*float64(outputPeriodAvg))
-		log.Println("[test] outputPeriodAvg:", outputPeriodAvg)
-	}
-}
-
-func TestPoolFull(t *testing.T) {
+func TestPool(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	workerProfiles := make([]string, 0)
 	for i := 0; i < 100; i++ {
 		workerProfiles = append(workerProfiles, fmt.Sprintf("w%d", i))
 	}
-	inputPeriod := 10 * time.Millisecond
-	jobDur := 500 * time.Millisecond
+	const inputPeriod = 10 * time.Millisecond
+	const jobDur = 500 * time.Millisecond
+	const successRate = 0.95
+	var pStats []stats
 	results := make(chan struct{})
 	p, err := NewPoolWithInit(len(workerProfiles), func(job Job[int], workerID int, connection struct{}) error {
 		worker := workerProfiles[workerID]
 		log.Printf("[test/worker%v] job%d started - attempt %d - worker %v\n", workerID, job.ID, job.Attempt, worker)
 		time.Sleep(jobDur)
-		if rand.Float32() > 0.95 {
+		if rand.Float32() > successRate {
 			return ErrorWrapRetryable(fmt.Errorf("job failure"))
 		}
 		results <- struct{}{}
@@ -94,11 +53,15 @@ func TestPoolFull(t *testing.T) {
 		time.Sleep(3 * jobDur)
 		log.Printf("[test/worker%v] disconnecting\n", workerID)
 		return nil
-	}, Retries(4), LoggerInfo(log.Default()), LoggerDebug(log.Default()))
+	}, Retries(4), LoggerInfo(log.Default()), LoggerDebug(log.Default()), monitor(func(s stats) {
+		pStats = append(pStats, s)
+	}))
 	if err != nil {
 		log.Printf("NewPool() failed: %s", err)
 		return
 	}
+	started := time.Now()
+	var stopped time.Time
 	go func() {
 		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
 		defer cancel()
@@ -110,6 +73,7 @@ func TestPoolFull(t *testing.T) {
 			p.Submit(i)
 		}
 		log.Println("[test] submitted jobs - calling p.StopAndWait()")
+		stopped = time.Now()
 		p.StopAndWait()
 		log.Println("[test] p.StopAndWait() returned")
 		close(results)
@@ -123,30 +87,53 @@ func TestPoolFull(t *testing.T) {
 		outputPeriodAvg = time.Duration(a*float64(outputPeriod) + (1-a)*float64(outputPeriodAvg))
 		log.Println("[test] outputPeriodAvg:", outputPeriodAvg)
 	}
+
+	pWorkersAvg, pWorkersStd := processStats(pStats, started.Add(30*time.Second), stopped)
+	fmt.Printf("pool workers: avg=%v std=%v\n", pWorkersAvg, pWorkersStd)
+	// expectedNumOfWorkers = effectiveJobDur/inputPeriod
+	// where effectiveJobDur = jobDur / successRate
+	// because each job is tried 1/successRate on average
+	expectedNumOfWorkers := float64(jobDur/inputPeriod) / successRate
+	if pWorkersAvg < 0.95*expectedNumOfWorkers {
+		t.Errorf("pWorkersAvg < 0.95*%v", expectedNumOfWorkers)
+	}
+	if pWorkersAvg > 1.1*expectedNumOfWorkers {
+		t.Errorf("pWorkersAvg > 1.1*%v", expectedNumOfWorkers)
+	}
+	// fail if standard deviation is too high
+	if pWorkersStd/pWorkersAvg > 0.05 {
+		t.Error("pWorkersStd/pWorkersAvg > 0.05")
+	}
 }
 
-func TestMultiplePoolsLongInputPeriod(t *testing.T) {
-	testMultiplePools(t, 20*time.Millisecond)
+func TestPipelineLongInputPeriod(t *testing.T) {
+	testPipeline(t, 20*time.Millisecond)
 }
 
-func TestMultiplePoolsMediumInputPeriod(t *testing.T) {
-	testMultiplePools(t, 10*time.Millisecond)
+func TestPipelineMediumInputPeriod(t *testing.T) {
+	testPipeline(t, 10*time.Millisecond)
 }
 
-func TestMultiplePoolsShortInputPeriod(t *testing.T) {
-	testMultiplePools(t, 0)
+func TestPipelineShortInputPeriod(t *testing.T) {
+	testPipeline(t, 0)
 }
 
-func testMultiplePools(t *testing.T, inputPeriod time.Duration) {
+func testPipeline(t *testing.T, inputPeriod time.Duration) {
 	const jobDur1 = 333 * time.Millisecond
 	const jobDur2 = 666 * time.Millisecond
 	const jobDur3 = 1000 * time.Millisecond
+
+	var p1Stats []stats
+	var p2Stats []stats
+	var p3Stats []stats
 
 	// stage 1: calculate square root
 	p1, err := NewPoolWithResults(100, func(job Job[float64], workerID int) (float64, error) {
 		time.Sleep(jobDur1)
 		return math.Sqrt(job.Payload), nil
-	}, Name("p1"), LoggerInfo(log.Default()), LoggerDebug(log.Default()))
+	}, Name("p1"), LoggerInfo(log.Default()), LoggerDebug(log.Default()), monitor(func(s stats) {
+		p1Stats = append(p1Stats, s)
+	}))
 	if err != nil {
 		log.Printf("NewPoolSimple() failed: %s", err)
 		return
@@ -156,7 +143,9 @@ func testMultiplePools(t *testing.T, inputPeriod time.Duration) {
 	p2, err := NewPoolWithResults(100, func(job Job[float64], workerID int) (float64, error) {
 		time.Sleep(jobDur2)
 		return -job.Payload, nil
-	}, Name("p2"), LoggerInfo(log.Default()), LoggerDebug(log.Default()))
+	}, Name("p2"), LoggerInfo(log.Default()), LoggerDebug(log.Default()), monitor(func(s stats) {
+		p2Stats = append(p2Stats, s)
+	}))
 	if err != nil {
 		log.Printf("NewPoolSimple() failed: %s", err)
 		return
@@ -166,7 +155,9 @@ func testMultiplePools(t *testing.T, inputPeriod time.Duration) {
 	p3, err := NewPoolWithResults(100, func(job Job[float64], workerID int) (string, error) {
 		time.Sleep(jobDur3)
 		return fmt.Sprintf("%.3f", job.Payload), nil
-	}, Name("p3"), LoggerInfo(log.Default()), LoggerDebug(log.Default()))
+	}, Name("p3"), LoggerInfo(log.Default()), LoggerDebug(log.Default()), monitor(func(s stats) {
+		p3Stats = append(p3Stats, s)
+	}))
 	if err != nil {
 		log.Printf("NewPoolSimple() failed: %s", err)
 		return
@@ -179,6 +170,7 @@ func testMultiplePools(t *testing.T, inputPeriod time.Duration) {
 	const a = 0.1
 	var inputPeriodAvg time.Duration
 	lastSubmitted := time.Now()
+	started := time.Now()
 	go func() {
 		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
 		defer cancel()
@@ -197,9 +189,9 @@ func testMultiplePools(t *testing.T, inputPeriod time.Duration) {
 			}
 			log.Printf("[test] submitting job%d - inputPeriodAvg: %v\n", i, inputPeriodAvg)
 			p1.Submit(float64(i))
-			inputPeriod := time.Since(lastSubmitted)
+			inputPeriodNow := time.Since(lastSubmitted)
 			lastSubmitted = time.Now()
-			inputPeriodAvg = time.Duration(a*float64(inputPeriod) + (1-a)*float64(inputPeriodAvg))
+			inputPeriodAvg = time.Duration(a*float64(inputPeriodNow) + (1-a)*float64(inputPeriodAvg))
 		}
 		log.Println("[test] submitted jobs - calling p1.StopAndWait()")
 		p1.StopAndWait()
@@ -213,6 +205,73 @@ func testMultiplePools(t *testing.T, inputPeriod time.Duration) {
 		outputPeriodAvg = time.Duration(a*float64(outputPeriod) + (1-a)*float64(outputPeriodAvg))
 		log.Println("[test] result:", result.Value, "outputPeriodAvg:", outputPeriodAvg)
 	}
+
+	p1WorkersAvg, p1WorkersStd := processStats(p1Stats, started.Add(30*time.Second), lastSubmitted)
+	p2WorkersAvg, p2WorkersStd := processStats(p2Stats, started.Add(30*time.Second), lastSubmitted)
+	p3WorkersAvg, p3WorkersStd := processStats(p3Stats, started.Add(30*time.Second), lastSubmitted)
+	fmt.Printf("p1 workers: avg=%v std=%v\n", p1WorkersAvg, p1WorkersStd)
+	fmt.Printf("p2 workers: avg=%v std=%v\n", p2WorkersAvg, p2WorkersStd)
+	fmt.Printf("p3 workers: avg=%v std=%v\n", p3WorkersAvg, p3WorkersStd)
+	// p1WorkersAvg should be about 1/3 of p3WorkersAvg
+	if p1WorkersAvg < 0.3*p3WorkersAvg {
+		t.Errorf("p1WorkersAvg < %v", 0.3*p3WorkersAvg)
+	}
+	if p1WorkersAvg > 0.4*p3WorkersAvg {
+		t.Errorf("p1WorkersAvg > %v", 0.4*p3WorkersAvg)
+	}
+	// p2WorkersAvg should be about 2/3 of p3WorkersAvg
+	if p2WorkersAvg < 0.6*p3WorkersAvg {
+		t.Errorf("p2WorkersAvg < %v", 0.6*p3WorkersAvg)
+	}
+	if p2WorkersAvg > 0.7*p3WorkersAvg {
+		t.Errorf("p2WorkersAvg > %v", 0.7*p3WorkersAvg)
+	}
+	var p3WorkersExpected float64
+	if inputPeriod > 0 {
+		p3WorkersExpected = float64(jobDur3 / inputPeriod)
+		if p3WorkersExpected > 100 {
+			p3WorkersExpected = 100
+		}
+	} else {
+		p3WorkersExpected = 100
+	}
+	if p3WorkersAvg < 0.9*p3WorkersExpected {
+		t.Errorf("p3WorkersAvg < 0.9*%v", p3WorkersExpected)
+	}
+	if p3WorkersAvg > 1.1*p3WorkersExpected {
+		t.Errorf("p3WorkersAvg > 1.1*%v", p3WorkersExpected)
+	}
+	// fail if standard deviation is too high
+	if p1WorkersStd/p1WorkersAvg > 0.1 {
+		t.Error("p1WorkersStd/p1WorkersAvg > 0.1")
+	}
+	if p2WorkersStd/p2WorkersAvg > 0.1 {
+		t.Error("p2WorkersStd/p2WorkersAvg > 0.1")
+	}
+	if p3WorkersStd/p3WorkersAvg > 0.05 {
+		t.Error("p3WorkersStd/p3WorkersAvg > 0.05")
+	}
+}
+
+// calculates the average and standard deviation of concurrency in the specified time period
+func processStats(statsArray []stats, from time.Time, to time.Time) (float64, float64) {
+	var workersSum int
+	var workersSumSq int
+	var n int
+	for _, s := range statsArray {
+		if s.Time.Before(from) {
+			continue
+		} else if s.Time.After(to) {
+			break
+		}
+		n++
+		workersSum += int(s.Concurrency)
+		workersSumSq += int(s.Concurrency * s.Concurrency)
+	}
+	nFloat := float64(n)
+	workersAvg := float64(workersSum) / nFloat
+	workersStd := math.Sqrt(float64(workersSumSq)/nFloat - math.Pow(float64(workersSum)/nFloat, 2))
+	return workersAvg, workersStd
 }
 
 func sleepCtx(ctx context.Context, dur time.Duration) bool {
